4f85b79f8ac6507e5ff588f9f99e84a4d344e8c5
paixaoariellll <paixaoariellll@gmail.com>
2022-09-16 12:13:44 -0300

    cookies config part-1

--- utils/Store.js
+++ utils/Store.js
@@ -4,6 +4,16 @@ import { useReducer } from 'react'
 const initialState = {
     cart: Cookies.get('cart') ? JSON.parse(Cookies.get('cart')) : { cartItems: [] }
 }
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CART_ADD_ITEM': {
+            const newItem = action.payload
+            const existItem = state.cart.cartItems.find((item) => item.slug === newItem.slug)
+            const cartItems = existItem ? state.cart.cartItems.map((item) => item.name === existItem ? newItem : item) : [...state.cart.cartItems, newItem]
+            Cookies.set('cart', JSON.stringify)
+        }
+    }
+}
 
 export default function StoreProvider({ children }) {
     const [state, dispatch] = useReducer(reducer, initialState)
