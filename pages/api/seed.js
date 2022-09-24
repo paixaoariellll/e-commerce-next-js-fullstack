101a108d13c00a75320b523171b57d0b345fd3e9
paixaoariellll <paixaoariellll@gmail.com>
2022-09-24 15:20:30 -0300

    bcrypt + mongodb + User + mongoose + api/seed

---
+++ pages/api/seed.js
@@ -0,0 +1,13 @@
+import User from "../../models/User"
+import data from "../../utils/data"
+import db from "../../utils/db"
+
+const handler = async (req, res) => {
+    await db.connect()
+    await User.deleteMany()
+    await User.insertMany(data.users)
+    await db.disconnect()
+    res.send({ message: 'pedido concluído com sucesso!' })
+}
+
+export default handler
--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+    users: [
+        {
+            name: 'Ariel',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'Milena',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+    ],
     products: [
         {
             name: 'Produto 1',
