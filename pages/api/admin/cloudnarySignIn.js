d77e9022a972bfa9cb3c6c607ac93dc0a5c9ca10
paixaoariellll <paixaoariellll@gmail.com>
2022-10-09 23:12:31 -0300

    remove cloudinary

--- pages/api/admin/cloudnarySignIn.js
+++ pages/api/admin/cloudnarySignIn.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary').v2
+/* const cloudinary = require('cloudinary').v2
 
 export default function signature(req, res) {
     const timestamp = Math.round(new Date().getTime() / 1000)
@@ -11,4 +11,4 @@ export default function signature(req, res) {
 
     res.statusCode = 200;
     res.json({ signature, timestamp })
-}
+} */
