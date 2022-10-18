5050e0acd60fb626756b1e571619acff788b9515
paixaoariellll <paixaoariellll@gmail.com>
2022-10-18 14:43:30 -0300

    SideBar

--- components/DashboardLinks.js
+++ components/DashboardLinks.js
@@ -3,29 +3,29 @@ import React from "react"
 
 export default function DashboardLinks() {
     return (
-        <div className="card !bg-blue-900 text-xl py-5 px-3">
-            <ul className="text-center align-middle items-center">
-                <li className="card p-2 hover:bg-blue-100 cursor-pointer">
-                    <i className="ri-admin-fill text-blue-700"></i>
+        <div className="card text-xl py-5 px-3">
+            <ul className="flex flex-col mt-3 text-center align-middle items-center">
+                <li className="py-2 text-blue-800">
+                    <i className="ri-admin-fill"></i>
                 </li>
-                <li className="card p-2 hover:bg-blue-100 cursor-pointer">
+                <li className="card px-2 py-2 cursor-pointer hover:bg-blue-700 text-blue-700 hover:text-white">
                     <Link href="/admin/dashboard">
-                        <span><i className="ri-dashboard-line text-blue-700"></i></span>
+                        <span><i className="ri-dashboard-line"></i></span>
                     </Link>
                 </li>
-                <li className="card p-2 hover:bg-blue-100 cursor-pointer">
+                <li className="card px-2 py-2 cursor-pointer hover:bg-blue-700 text-blue-700 hover:text-white">
                     <Link href="/admin/orders">
-                        <span><i className="ri-file-list-3-line text-blue-700"></i></span>
+                        <span><i className="ri-file-list-3-line"></i></span>
                     </Link>
                 </li>
-                <li className="card p-2 hover:bg-blue-100 cursor-pointer">
+                <li className="card px-2 py-2 cursor-pointer hover:bg-blue-700 text-blue-700 hover:text-white">
                     <Link href="/admin/products">
-                        <span> <i className="ri-list-unordered text-blue-700"></i></span>
+                        <span><i className="ri-list-unordered"></i></span>
                     </Link>
                 </li>
-                <li className="card p-2 hover:bg-blue-100 cursor-pointer">
+                <li className="card px-2 py-2 cursor-pointer hover:bg-blue-700 text-blue-700 hover:text-white">
                     <Link href="/admin/users">
-                        <span> <i className="ri-user-settings-line text-blue-700"></i></span>
+                        <span><i className="ri-user-settings-line"></i></span>
                     </Link>
                 </li>
             </ul>
