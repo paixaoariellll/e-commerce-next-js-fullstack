2dc766d39ec048447ad947a8e17fb73f6948bd62
paixaoariellll <paixaoariellll@gmail.com>
2022-10-23 18:55:39 -0300

    teste

--- pages/teste.js
+++ pages/teste.js
@@ -1,25 +1,71 @@
-import Image from "next/image";
-import React from "react";
-import AliceCarousel from "react-alice-carousel";
-import "react-alice-carousel/lib/alice-carousel.css";
-import Layout from "../components/Layout";
-import img1 from "../public/img/Spider-man.jpeg";
+import { Carousel } from '@mantine/carousel';
+import React from 'react';
+import { useRef } from 'react';
+import Autoplay from 'embla-carousel-autoplay';
+import Image from 'next/image';
+import img1 from '../public/img/God-of-war-ragnarok.jpg'
+import img2 from '../public/img/Death-stranding.jpg'
+import img3 from '../public/img/Spider-man.jpeg'
+import img4 from '../public/img/Gears-5.jpeg'
+import img5 from '../public/img/Dark-souls-3.jpeg'
+import img6 from '../public/img/Fifa-23.jpg'
+import Layout from '../components/Layout';
 
-const handleDragStart = (e) => e.preventDefault();
-
-/* const items = [
-    <Image src={img1} alt="image" width={200} height={200} onDragStart={handleDragStart} role="presentation" />,
-    <Image src={img1} alt="image" width={200} height={200} onDragStart={handleDragStart} role="presentation" />,
-    <Image src={img1} alt="image" width={200} height={200} onDragStart={handleDragStart} role="presentation" />,
-]; */
-
-export default function teste({ items }) {
+export default function teste() {
+    const autoplay = useRef(Autoplay({ delay: 5000 }));
     return (
         <Layout>
-            <div className="card p-5">
-                <AliceCarousel mouseTracking items={items} />
-            </div>
-        </Layout>
-    );
-}
+            <div className='card cursor-move p-5'>
+                <h1 className='text-4xl text-center text-blue-800 m-5'>Jogos da Semana</h1>
+                <Carousel
+                    plugins={[autoplay.current]}
+                    onMouseEnter={autoplay.current.stop}
+                    onMouseLeave={autoplay.current.reset}
+                    slideSize="33.3%"
+                    align="center"
+                    height={400}
+                    slideGap="md"
+                    controlsOffset="xs"
+                    controlSize={50}
+                    orientation="horizontal"
+                    loop
+                    withIndicators
+                    styles={{
+                        indicator: {
+                            width: 100,
+                            height: 10,
+                            color: 'red',
+                            backgroundColor: 'red',
+                            transition: 'width 250ms ease-in-out',
+                            '&[data-active]': {
+                                width: 300,
+                                background: 'blue',
+                            },
+                        },
+                    }}
+                    withControls={false}
 
+                >
+                    <Carousel.Slide>
+                        <Image src={img1} alt="imagem" width={500} height={500} unoptimized />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image src={img2} alt="imagem" width={500} height={500} unoptimized />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image src={img3} alt="imagem" width={500} height={500} unoptimized />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image src={img4} alt="imagem" width={500} height={500} unoptimized />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image src={img5} alt="imagem" width={500} height={500} unoptimized />
+                    </Carousel.Slide>
+                    <Carousel.Slide>
+                        <Image src={img6} alt="imagem" width={500} height={500} unoptimized />
+                    </Carousel.Slide>
+                </Carousel>
+            </div>
+        </Layout >
+    )
+}
