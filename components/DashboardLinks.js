a4aad621b036cde738c6eaa2a4c9c5b29db5bd4e
paixaoariellll <paixaoariellll@gmail.com>
2022-11-01 14:43:51 -0300

    line

--- components/DashboardLinks.js
+++ components/DashboardLinks.js
@@ -8,6 +8,7 @@ export default function DashboardLinks() {
                 <li className="py-0 text-2xl text-blue-800">
                     <i className="ri-admin-fill"></i>
                 </li>
+                <hr className="mt-1 mb-4 w-full flex flex-1 border-t border border-t-blue-700 "></hr>
                 <Link href="/admin/dashboard">
                     <li className="card px-2 py-0 cursor-pointer !rounded-full hover:bg-blue-700 text-blue-700 hover:text-white">
                         <span className="relative top-0.5"><i className="ri-dashboard-line"></i></span>
