3af2856e5e5b2634093caa29233bee094541915e
paixaoariellll <paixaoariellll@gmail.com>
2022-11-04 18:15:16 -0300

    origin

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-app.use(cors())
+app.use(cors({ origin: "*" }))
 
 app.get('/', async (req, res, next) => {
   res.send({ message: 'Tá funcionando já! ❤️' });
