fd59628140f6583f07572e8c5b0f749a27c44280
paixaoariellll <paixaoariellll@gmail.com>
2022-12-05 01:12:28 -0300

    Apresentação 5

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import React, { useEffect, useState } from "react";
@@ -8,67 +9,35 @@ import ControllerLogo from "../public/images/controller.svg";
 import Ariel from "../public/imgUser/a.jpg";
 import Carlos from "../public/imgUser/c.jpg";
 import Joao from "../public/imgUser/j.jpg";
+import Cloud from "../public/images/cloudinary.svg";
+import Figma from "../public/images/figma.svg";
+import Js from "../public/images/javascript.svg";
+import Mdb from "../public/images/mongodb.svg";
+import Next from "../public/images/nextjs.svg";
+import Node from "../public/images/nodejs.svg";
+import Tw from "../public/images/tailwindcss.svg";
+import Git from "../public/images/github.svg";
+import Ln from "../public/images/linkedin.svg";
 
 function AboutScreen() {
-  const [loopNumi, setLoopNumi] = useState(0);
   const [loopNumj, setLoopNumj] = useState(0);
-  const [isDeletingi, setIsDeletingi] = useState(false);
   const [isDeletingj, setIsDeletingj] = useState(false);
-  const [texti, setTexti] = useState('');
   const [textj, setTextj] = useState('');
-  const [deltai, setDeltai] = useState();
   const [deltaj, setDeltaj] = useState();
-  const [indexi, setIndexi] = useState(1);
   const [indexj, setIndexj] = useState(2);
-  const toRotatei = ["GameOn", "Ariel Paixão", "Carlos Júnior", "João Machado", "Apenas uma pessoas qualquer com a capacidade de insistir... "];
-  const toRotatej = ["Sejam Bem vindos!      ", "Ariel Paixão      🫡   ", "Carlos Júnior      🫡   ", "João Machado      🫡   ", "GameOn    🎮   "];
-
-  console.log(indexi, indexj);
-
-  const periodi = 1500;
+  const toRotatej = ["Sejam bem-vindos!      ", "Ariel Paixão      🫡   ", "Carlos Júnior      🫡   ", "João Machado      🫡   ", "GameOn    🎮   "];
   const periodj = 100;
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, deltai);
-    return () => { clearInterval(ticker) };
-  }, [texti])
-
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
     }, deltaj);
     return () => { clearInterval(ticker) };
   }, [textj])
-
   const tick = () => {
-    let i = loopNumi % toRotatei.length;
     let j = loopNumj % toRotatej.length;
-    let fullTexti = toRotatei[i];
     let fullTextj = toRotatej[j];
-    let updatedTexti = isDeletingi ? fullTexti.substring(0, texti.length - 1) : fullTexti.substring(0, texti.length + 1);
     let updatedTextj = isDeletingj ? fullTextj.substring(0, textj.length - 1) : fullTextj.substring(0, textj.length + 1);
-
-    setTexti(updatedTexti);
     setTextj(updatedTextj);
-
-    if (isDeletingi) {
-      setDeltai(prevDelta => prevDelta / 1);
-    }
-    if (!isDeletingi && updatedTexti === fullTexti) {
-      setIsDeletingi(true);
-      setIndexi(prevIndex => prevIndex - 1);
-      setDeltai(periodi);
-    } else if (isDeletingi && updatedTexti === '') {
-      setIsDeletingi(false);
-      setLoopNumi(loopNumi + 1);
-      setIndexi(1);
-      setDeltai(200);
-    } else {
-      setIndexi(prevIndex => prevIndex + 1);
-    }
-
     if (!isDeletingj && updatedTextj === fullTextj) {
       setIsDeletingj(true);
       setIndexj(prevIndex => prevIndex - 1);
@@ -84,7 +53,7 @@ function AboutScreen() {
   }
   return (
     <Layout title="Sobre - GameOn">
-      <section className="">
+      <section>
         <TrackVisibility>
           {
             ({ isVisible }) =>
@@ -126,13 +95,25 @@ function AboutScreen() {
                       </a>
                       .
                     </div>
+                    <div className="card p-5">
+                      <ul>
+                        <li className="mt-1 flex justify-between">
+                          <Image src={Cloud} className="!p-1" width={50} height={50} alt="redes" unoptimized />
+                          <Image src={Figma} className="!p-1" width={45} height={50} alt="redes" unoptimized />
+                          <Image src={Js} className="!p-1" width={45} height={50} alt="redes" unoptimized />
+                          <Image src={Mdb} className="!p-1" width={45} height={50} alt="redes" unoptimized />
+                          <Image src={Next} className="!p-1" width={45} height={50} alt="redes" unoptimized />
+                          <Image src={Node} className="!p-1" width={45} height={50} alt="redes" unoptimized />
+                          <Image src={Tw} className="!p-1" width={45} height={50} alt="redes" unoptimized />
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
           }
         </TrackVisibility >
       </section>
-
       <div className="my-5">
         <div className="container h-screen flex flex-col justify-center items-center bg-blue-100">
           <TrackVisibility>
@@ -164,77 +145,112 @@ function AboutScreen() {
       </div>
       <div className="my-5 ">
         <div className="w-full p-16 h-screen gap-x-10 flex justify-between items-center bg-blue-100">
-          <TrackVisibility className=" justify-center items-center h-full flex w-full">
+          <TrackVisibility className="justify-center items-center flex">
             {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeInLeft animate__delay-1s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <Image src={Ariel} alt="Ariel Paixão" width={600} height={600} unoptimized title="Ariel" />
+                <Image src={Ariel} className="rounded-full" alt="Ariel Paixão" width={600} height={600} unoptimized title="Ariel" />
               </div>}
           </TrackVisibility>
-          <TrackVisibility className="h-full flex w-full">
-            {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeInRight animate__delay-0s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <div className="pt-20">
+          <TrackVisibility className="h-fit flex w-full">
+            {
+              ({ isVisible }) => (
+                <div className={isVisible ? "animate__animated animate__fadeInRight animate__delay-0s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
                   <h1 className="text-5xl card p-4 text-blue-800 text-center"> Ariel Afonso Coelho Paixão </h1>
                   <div className="pt-10 text-3xl card p-5">
                     <p className="text-center pb-5 text-4xl">
                       Desenvolvedor
                     </p>
-                    <p>
-                      Procurei as ferramentas necessárias para a criação do projeto e arquitetei os processos das sprints para que pudéssemos ter uma organização quanto aos prazos das entregas
+                    <p className="text-justify">
+                      Desenvolvi as principais funcionalidades do sistema e implementei as regras de negócios e validações necessárias.
+                      Realizei também a integração da aplicação com o PayPal e fiz o <em>deploy</em> do projeto pelo Vercel.
                     </p>
                   </div>
+                  <div className="card p-5">
+                    <h1 className="text-2xl">Entre em contato:</h1>
+                    <div className="flex flex-wrap justify-start">
+                      <Link href="https://www.github.com/paixaoariellll" target="_blank" className="cursor-pointer" >
+                        <Image src={Git} className="!p-1" width={45} height={50} alt="" unoptimized />
+                      </Link>
+                      <Link href="https://www.linkedin.com/in/ariel-paixao" target="_blank" className="cursor-pointer">
+                        <Image src={Ln} className="!p-1" width={45} height={50} alt="" unoptimized />
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-              </div>}
+              )
+            }
           </TrackVisibility>
         </div>
       </div>
-      <div className="my-5 ">
+      <div className="my-5">
         <div className="w-full p-16 h-screen gap-x-10 flex justify-between items-center bg-blue-100">
-          <TrackVisibility className="h-full flex w-full">
+          <TrackVisibility className="h-fit flex w-full">
             {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeInRight animate__delay-1s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <div className="pt-20">
-                  <h1 className="text-5xl card text-blue-800 text-center"> Carlos Marques de Oliveira Júnior</h1>
-                  <div className="pt-10 text-3xl card p-5">
-                    <p className="text-center pb-5 text-4xl">
-                      Desenvolvedor
-                    </p>
-                    <p>
-                      Auxiliei na criação dos protótipos, programei algumas telas e preenchi o banco de dados com informações reais.
-                      Além disso ajudei na criação das ideias que nossa equipe desenvolveu.
-                    </p>
+                <h1 className="text-5xl card text-blue-800 text-center"> Carlos Marques de Oliveira Júnior</h1>
+                <div className="pt-10 text-3xl card p-5">
+                  <p className="text-center pb-5 text-4xl">
+                    Desenvolvedor
+                  </p>
+                  <p className="text-justify">
+                    Auxiliei na elicitação de requisitos, ajudei na codificação e na criação do banco de dados.
+                  </p>
+                  <p className="text-justify">
+                    Além disso, fiz uma análise dos concorrentes para ajudar na criação de novas ideias que nossa equipe desenvolveu.
+                  </p>
+                </div>
+                <div className="card p-5">
+                  <h1 className="text-2xl">Entre em contato:</h1>
+                  <div className="flex flex-wrap justify-start cursor-pointer">
+                    <Link href="https://github.com/CarlosDevMarques">
+                      <Image src={Git} className="!p-1" width={45} height={50} alt="" unoptimized />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/carlos-marques-4b6aa31b5/">
+                      <Image src={Ln} className="!p-1" width={45} height={50} alt="" unoptimized />
+                    </Link>
                   </div>
                 </div>
               </div>}
           </TrackVisibility>
-          <TrackVisibility className=" justify-center items-center h-full flex w-full">
+          <TrackVisibility className=" justify-center items-center flex ">
             {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeInLeft animate__delay-2s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <Image src={Carlos} alt="Ariel Paixão" width={600} height={600} unoptimized title="Ariel" />
+                <Image src={Carlos} className="rounded-full" alt="Ariel Paixão" width={600} height={600} unoptimized title="Ariel" />
               </div>}
           </TrackVisibility>
         </div>
       </div>
       <div className="my-5">
         <div className="w-full p-16 h-screen gap-x-10 flex justify-between items-center bg-blue-100">
-          <TrackVisibility className=" justify-center items-center h-full flex w-full">
+          <TrackVisibility className=" justify-center items-center flex ">
             {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeInLeft animate__delay-2s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <Image src={Joao} alt="Ariel Paixão" width={600} height={600} unoptimized title="Ariel" />
+                <Image src={Joao} className="rounded-full" alt="Ariel Paixão" width={600} height={600} unoptimized title="Ariel" />
               </div>}
           </TrackVisibility>
-          <TrackVisibility className="h-full flex w-full">
+          <TrackVisibility className="h-fit flex w-full">
             {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeInRight animate__delay-1s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <div className="pt-20">
+                <div className="">
                   <h1 className="text-5xl text-center card p-5 text-blue-800"> João Lucas Coelho Machado </h1>
-                  <div className="pt-10 text-3xl  p-5  card">
+                  <div className="pt-10 text-3xl p-5 card">
                     <p className="text-center pb-5 text-4xl">
-                      Scrum Master
+                      Líder Técnico
                     </p>
-                    <p className="">
-                      Mandei, meus escravos fizeram!
+                    <p className="text-justify">
+                      Ajuda na codificação de forma geral, bem como na implementação de medidas de segurança do sistema. Ademais, elaborei as documentações e fui o responsável por manter o repositório do projeto.
                     </p>
+                  </div>
+                </div>
+                <div className="card p-5">
+                  <h1 className="text-2xl">Entre em contato:</h1>
+                  <div className="flex flex-wrap justify-start">
+                    <a href="https://www.github.com/machado-joao" className="cursor-pointer">
+                      <Image src={Git} className="" width={45} height={50} alt="" unoptimized />
+                    </a>
+                    <a href="https://www.linkedin.com/in/machado-joao">
+                      <Image src={Ln} className="!p-1" width={45} height={50} alt="" unoptimized />
+                    </a>
                   </div>
                 </div>
               </div>}
@@ -243,65 +259,33 @@ function AboutScreen() {
       </div>
       <div className="my-5">
         <div className="w-full p-16 h-screen gap-x-10 flex justify-between items-center bg-blue-100">
-          <TrackVisibility className="h-full flex w-full">
+          <TrackVisibility className="h-fit flex w-full">
             {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeInRight animate__delay-4s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <div className="pt-20">
-                  <h1 className="text-5xl text-center !bg-red-100 card p-5 text-blue-800"> Problema </h1>
+              <div className={isVisible ? "animate__animated animate__fadeInLeft animate__delay-0s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
+                <div className="">
+                  <h1 className="text-5xl text-center !bg-red-600 card p-5 text-white"> Problema </h1>
                   <div className="pt-10 text-3xl card p-5">
-                    <p className="text-center pb-5 text-4xl">
-                      Resumo
-                    </p>
                     <ul className="list-disc p-5">
-                      <li>Jogos eletrônicos de mídia física;</li>
-                      <li>Plataforma exclusiva;</li>
-                      <li>Ambiente customizado para o público-alvo;</li>
+                      <li>Plataforma exclusiva para <em>gamers</em>;</li>
                       <li>Interface intuitiva;</li>
-                      <li>Segurança de ponta a ponta.</li>
+                      <li>Etapas facilitadas;</li>
+                      <li>Segurança.</li>
                     </ul>
                   </div>
                 </div>
               </div>}
           </TrackVisibility>
-          <TrackVisibility className="h-full flex w-full">
+          <TrackVisibility className="h-fit flex w-full">
             {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeInRight animate__delay-4s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <div className="pt-20">
-                  <h1 className="text-5xl text-center !bg-green-100 card p-5 text-blue-800"> Objetivo </h1>
+              <div className={isVisible ? "animate__animated animate__fadeInRight animate__delay-2s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
+                <div className="">
+                  <h1 className="text-5xl text-center !bg-blue-800 card p-5 text-white"> Objetivo </h1>
                   <div className="pt-10 text-3xl card p-5">
-                    <p className="text-center pb-5 text-4xl">
-                      Resumo
-                    </p>
                     <ul className="list-disc p-5">
-                      <li>Jogos eletrônicos de mídia física;</li>
-                      <li>Plataforma exclusiva;</li>
-                      <li>Ambiente customizado para o público-alvo;</li>
-                      <li>Interface intuitiva;</li>
-                      <li>Segurança de ponta a ponta.</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>}
-          </TrackVisibility>
-        </div>
-      </div>
-      <div className="my-5">
-        <div className="container h-screen flex flex-col justify-center items-center bg-blue-100">
-          <TrackVisibility className="h-full flex w-full">
-            {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeInRight animate__delay-4s" : "animate__animated animate__fadeOutUp animate__delay-0.5s"}>
-                <div className="pt-20">
-                  <h1 className="text-5xl text-center !bg-green-100 card p-5 text-blue-800"> Objetivo </h1>
-                  <div className="pt-10 text-3xl card p-5">
-                    <p className="text-center pb-5 text-4xl">
-                      Resumo
-                    </p>
-                    <ul className="list-disc p-5">
-                      <li>Jogos eletrônicos de mídia física;</li>
-                      <li>Plataforma exclusiva;</li>
-                      <li>Ambiente customizado para o público-alvo;</li>
-                      <li>Interface intuitiva;</li>
-                      <li>Segurança de ponta a ponta.</li>
+                      <li>Aplicação de metodologias ágeis;</li>
+                      <li>Realizar a modelagem do sistema;</li>
+                      <li>Desenvolver um <em>e-commerce</em>;</li>
+                      <li>Fazer a implantação do sistema.</li>
                     </ul>
                   </div>
                 </div>
